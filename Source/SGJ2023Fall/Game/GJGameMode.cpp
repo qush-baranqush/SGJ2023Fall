f923a57725ff7d57bb274e3d5a4877b3a812c4b0
reddification <reddification@gmail.com>
2023-11-04 17:27:56 +0400

    Day-light change, added gameplay message router

---
+++ Source/SGJ2023Fall/Game/GJGameMode.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GJGameMode.h"
+
+#include "WorldTimeComponent.h"
+
+
+// Sets default values
+AGJGameMode::AGJGameMode()
+{
+	WorldTimeComponent = CreateDefaultSubobject<UWorldTimeComponent>(TEXT("WorldTime"));
+}
--- Source/SGJ2023Fall/SGJ2023Fall.Build.cs
+++ Source/SGJ2023Fall/SGJ2023Fall.Build.cs
@@ -9,5 +9,6 @@ public class SGJ2023Fall : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayMessageRuntime", "DeveloperSettings", "GameplayTags" });
 	}
 }
