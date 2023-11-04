f4c5a45971074c5082cd97401a6c92575c72d158
reddification <reddification@gmail.com>
2023-11-04 17:37:51 +0400

    Interactable actor stub

---
+++ Source/SGJ2023Fall/Actors/InteractableActor.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "GameFramework/Actor.h"
+#include "InteractableActor.generated.h"
+
+UCLASS(BlueprintType)
+class SGJ2023FALL_API AInteractableActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AInteractableActor();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayTag ItemTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayTagContainer SpecificTags;
+};
